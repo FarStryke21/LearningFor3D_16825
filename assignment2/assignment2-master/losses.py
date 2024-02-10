7e74903da42555752b00c49e8e6ba9da29126452
Aman Chulawala <aman.chulawala@gmail.com>
2024-02-10 10:12:55 -0500

    Model Update

--- assignment2/assignment2-master/losses.py
+++ assignment2/assignment2-master/losses.py
@@ -1,20 +1,40 @@
 import torch
+import pytorch3d as p3d
 
 # define losses
 def voxel_loss(voxel_src,voxel_tgt):
 	# voxel_src: b x h x w x d
 	# voxel_tgt: b x h x w x d
-	# loss = 
+	# Convert src and tgt to a 2d shape of b and number of voxels
+	voxel_src_new = voxel_src.view(voxel_src.shape[0],-1)
+	voxel_tgt_new = voxel_tgt.view(voxel_tgt.shape[0],-1)
+
+	# Implement loss
+	loss = torch.nn.BCELoss(voxel_src_new,voxel_tgt_new)
 	# implement some loss for binary voxel grids
 	return loss
 
 def chamfer_loss(point_cloud_src,point_cloud_tgt):
-	# point_cloud_src, point_cloud_src: b x n_points x 3  
-	# loss_chamfer = 
+	# point_cloud_src, point_cloud_src: b x n_points x 3 
+	# Compute pairwise distances
+	dist_src_tgt, src_tgt_idx, _ = p3d.ops.knn.knn_points(point_cloud_src, point_cloud_tgt, K=1) # (batch_size, n_points, 1)
+	dist_tgt_src, tgt_src_idx, _ = p3d.ops.knn.knn_points(point_cloud_tgt, point_cloud_src, K=1) # (batch_size, n_points, 1)
+	
+	
+	loss_chamfer = torch.sum(torch.stack([dist_src_tgt, dist_tgt_src]))
+
 	# implement chamfer loss from scratch
 	return loss_chamfer
 
 def smoothness_loss(mesh_src):
 	# loss_laplacian = 
 	# implement laplacian smoothening loss
+	# Get the vertex positions of the source mesh
+	V = mesh_src.verts_packed()
+
+	# Compute the Laplacian operator on the vertex positions
+	L = mesh_src.laplacian_packed()
+
+	# Compute the smoothness loss as the mean of the squared Laplacian operator
+	loss_laplacian = torch.square(torch.linalg.norm(torch.matmul(L, V)))
 	return loss_laplacian
--- assignment2/assignment2-master/model.py
+++ assignment2/assignment2-master/model.py
@@ -6,6 +6,60 @@ import torch
 from pytorch3d.utils import ico_sphere
 import pytorch3d
 
+
+class ReshapeLayer(nn.Module):
+    def __init__(self, shape):
+        super(ReshapeLayer, self).__init__()
+        self.shape = shape
+
+    def forward(self, x):
+        return x.reshape(self.shape)
+    
+class ResnetBlockFC(nn.Module):
+    ''' Fully connected ResNet Block class.
+
+    Args:
+        size_in (int): input dimension
+        size_out (int): output dimension
+        size_h (int): hidden dimension
+    '''
+
+    def __init__(self, size_in, size_out=None, size_h=None):
+        super().__init__()
+        # Attributes
+        if size_out is None:
+            size_out = size_in
+
+        if size_h is None:
+            size_h = min(size_in, size_out)
+
+        self.size_in = size_in
+        self.size_h = size_h
+        self.size_out = size_out
+        # Submodules
+        self.fc_0 = nn.Linear(size_in, size_h)
+        self.fc_1 = nn.Linear(size_h, size_out)
+        self.actvn = nn.ReLU()
+
+        if size_in == size_out:
+            self.shortcut = None
+        else:
+            self.shortcut = nn.Linear(size_in, size_out, bias=False)
+        # Initialization
+        nn.init.zeros_(self.fc_1.weight)
+
+    def forward(self, x):
+        net = self.fc_0(self.actvn(x))
+        dx = self.fc_1(self.actvn(net))
+
+        if self.shortcut is not None:
+            x_s = self.shortcut(x)
+        else:
+            x_s = x
+
+        return x_s + dx
+
+
 class SingleViewto3D(nn.Module):
     def __init__(self, args):
         super(SingleViewto3D, self).__init__()
@@ -20,15 +74,41 @@ class SingleViewto3D(nn.Module):
         if args.type == "vox":
             # Input: b x 512
             # Output: b x 32 x 32 x 32
-            pass
             # TODO:
-            # self.decoder =             
+            # self.decoder =  
+            self.decoder = nn.Sequential(
+                nn.Linear(512, 2048), # b x 512 -> # b x 2048
+                ReshapeLayer((-1, 256, 2, 2, 2)), # b x 2048 -> b x 256 x 2 x 2 x 2
+                nn.ConvTranspose3d(256, 128, kernel_size=4, stride=2, bias=False, padding=1), # b x 256 x 2 x 2 x 2 -> b x 128 x 2 x 2 x 2
+                nn.BatchNorm3d(128),
+                nn.ReLU(),
+                nn.ConvTranspose3d(128, 64, kernel_size=4, stride=2, bias=False, padding=1), # b x 128 x 2 x 2 x 2 -> b x 64 x 2 x 2 x 2
+                nn.BatchNorm3d(64),
+                nn.ReLU(),
+                nn.ConvTranspose3d(64, 32, kernel_size=4, stride=2, bias=False, padding=1), # b x 64 x 2 x 2 x 2 -> b x 32 x 2 x 2 x 2
+                nn.BatchNorm3d(32),
+                nn.ReLU(),
+                nn.ConvTranspose3d(32, 8, kernel_size=4, stride=2, bias=False, padding=1), # b x 32 x 2 x 2 x 2 -> b x 8 x 2 x 2 x 2
+                nn.BatchNorm3d(8),
+                nn.ReLU(),
+                nn.ConvTranspose3d(8, 1, kernel_size=1, bias=False), # b x 8 x 2 x 2 x 2 -> b x 1 x 2 x 2 x 2
+                nn.Sigmoid()
+            )   
+           
         elif args.type == "point":
             # Input: b x 512
             # Output: b x args.n_points x 3  
             self.n_point = args.n_points
             # TODO:
-            # self.decoder =             
+            # self.decoder = 
+            self.decoder = nn.Sequential(
+                nn.Linear(512, self.n_point),
+                nn.LeakyReLU(),
+                nn.Linear(self.n_point, self.n_point*3),
+                ReshapeLayer((-1, args.n_points, 3)),
+                nn.Tanh()
+            )
+                            
         elif args.type == "mesh":
             # Input: b x 512
             # Output: b x mesh_pred.verts_packed().shape[0] x 3  
@@ -36,7 +116,29 @@ class SingleViewto3D(nn.Module):
             mesh_pred = ico_sphere(4, self.device)
             self.mesh_pred = pytorch3d.structures.Meshes(mesh_pred.verts_list()*args.batch_size, mesh_pred.faces_list()*args.batch_size)
             # TODO:
-            # self.decoder =             
+            # self.decoder =
+            self.decoder = nn.Sequential(
+                nn.Linear(512, 3*mesh_pred.verts_packed().shape[0]),
+                nn.Tanh()
+            )
+
+        elif args.type == 'implicit':
+            # Input : b x (512 + num_coords x 3)
+            # Output : b x num_coords x 1
+            
+            # Start with a simple implementation of a network that predicts the occupancy given the image feature and a 3d coordinate as input. 
+            # You will need to create a meshgrid of 32x32x32 in the normalized coordinate space of (-1,1)^3 to predict the full occupancy output.
+            # It does not perform any form of normalization.
+            
+            self.decoder =  nn.Sequential(
+                ResnetBlockFC(128, 128, size_h=None),
+                ResnetBlockFC(128, 128, size_h=None),
+                ResnetBlockFC(128, 128, size_h=None),
+                ResnetBlockFC(128, 128, size_h=None),
+                ResnetBlockFC(128, 128, size_h=None),
+                nn.ReLU(),
+                nn.Linear(128, 1),
+            )             
 
     def forward(self, images, args):
         results = dict()
@@ -55,17 +157,32 @@ class SingleViewto3D(nn.Module):
         # call decoder
         if args.type == "vox":
             # TODO:
-            # voxels_pred =             
+            voxels_pred = self.decoder(encoded_feat)   # b x 1 x 2 x 2 x 2
+            print(f'voxel pred : {voxels_pred.shape}')            
             return voxels_pred
 
         elif args.type == "point":
             # TODO:
-            # pointclouds_pred =             
+            pointclouds_pred = self.decoder(encoded_feat)   # 2, 5000, 3            
             return pointclouds_pred
 
         elif args.type == "mesh":
             # TODO:
-            # deform_vertices_pred =             
+            deform_vertices_pred = self.decoder(encoded_feat)                          
             mesh_pred = self.mesh_pred.offset_verts(deform_vertices_pred.reshape([-1,3]))
             return  mesh_pred          
 
+        elif args.type == "implicit":
+            self.n_coords = args.n_coords # default=2048
+            self.fc_p = nn.Linear(3*self.n_coords, 128)
+            self.fc_c = nn.Linear(512, 128)
+
+            self.sample_p = 2 * torch.rand((B, self.num_coords * 3)) - 1
+
+            net = self.fc_p(self.sample_p)
+            net_c = self.fc_c(encoded_feat).unsqueeze(1)
+            net = net + net_c
+
+            implicit_pred = self.decoder(net).squeeze(-1)
+
+            return implicit_pred
