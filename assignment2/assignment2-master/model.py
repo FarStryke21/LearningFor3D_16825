430e26ea5f3c13884609ad599f6b0305c6e54d0f
Aman Chulawala <aman.chulawala@gmail.com>
2024-02-13 10:26:56 -0500

    implicit tweaks

--- assignment2/assignment2-master/model.py
+++ assignment2/assignment2-master/model.py
@@ -197,7 +197,9 @@ class SingleViewto3D(nn.Module):
             encoded_feat = nn.ReLU()(encoded_feat)
 
             # Process coordinates
-            coordinates = self.fc_coords(coordinates)
+            self.n_coords = args.n_coords
+            self.sample_p = 2 * torch.rand((B, self.num_coords * 3)) - 1
+            coordinates = self.fc_coords(self.sample_p)
             coordinates = nn.ReLU()(coordinates)
 
             # Concatenate encoded features and coordinates
