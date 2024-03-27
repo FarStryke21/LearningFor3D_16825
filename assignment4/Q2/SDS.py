e5372a0f1935784e7b183f3e69f6cb2f84ec872b
Ubuntu <ubuntu@ip-172-31-79-50.ec2.internal>
2024-03-27 02:10:40 +0000

    no output

--- assignment4/Q2/SDS.py
+++ assignment4/Q2/SDS.py
@@ -151,7 +151,8 @@ class SDS:
             with torch.no_grad():
             ### YOUR CODE HERE ###  
                 eps = torch.randn_like(latents).to(self.device)
-                new_latent = torch.sqrt(self.alphas[t]) * latents + torch.sqrt(1 - self.alphas[t]) * eps
+                #new_latent = torch.sqrt(self.alphas[t]) * latents + torch.sqrt(1 - self.alphas[t]) * eps
+                new_latent = self.scheduler.add_noise(latents, eps, t)
                 eps_hat = self.unet(new_latent, t, encoder_hidden_states = text_embeddings).sample
 
                 if text_embeddings_uncond is not None and guidance_scale != 1:
