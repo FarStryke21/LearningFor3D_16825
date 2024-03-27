472a0b2bd9100881341228f3c0c357d3435a129b
Aman Chulawala <aman.chulawala@gmail.com>
2024-03-27 18:20:46 -0400

    Update Q23_nerf_optimization.py

--- assignment4/Q2/Q23_nerf_optimization.py
+++ assignment4/Q2/Q23_nerf_optimization.py
@@ -333,7 +333,8 @@ if __name__ == "__main__":
     os.makedirs(output_dir, exist_ok=True)
 
     # initialize SDS
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cpu")
     sds = SDS(sd_version="2.1", device=device, output_dir=output_dir)
 
     # optimize a NeRF model
