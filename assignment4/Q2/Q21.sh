cbee98eda24feea03e3cd4803d7ac743d6a3f62b
Ubuntu <ubuntu@ip-172-31-79-50.ec2.internal>
2024-03-27 04:22:43 +0000

    x

--- assignment4/Q2/Q21.sh
+++ assignment4/Q2/Q21.sh
@@ -1,10 +1,12 @@
-python python Q21_image_optimization.py --prompt a hamburger --sds_guidance 0
-python python Q21_image_optimization.py --prompt a hamburger --sds_guidance 1
-python python Q21_image_optimization.py --prompt a standing corgi dog --sds_guidance 0
-python python Q21_image_optimization.py --prompt a standing corgi dog --sds_guidance 1
-python python Q21_image_optimization.py --prompt a robot arm --sds_guidance 0
-python python Q21_image_optimization.py --prompt a robot arm --sds_guidance 1
-python python Q21_image_optimization.py --prompt a pikachu --sds_guidance 0
-python python Q21_image_optimization.py --prompt a pikachu --sds_guidance 1
-python python Q21_image_optimization.py --prompt a dragon --sds_guidance 0
-python python Q21_image_optimization.py --prompt a dragon --sds_guidance 1
+#!/bin/bash
+
+python Q21_image_optimization.py --prompt "a hamburger" --sds_guidance 0
+python Q21_image_optimization.py --prompt "a hamburger" --sds_guidance 1
+python Q21_image_optimization.py --prompt "a standing corgi dog" --sds_guidance 0
+python Q21_image_optimization.py --prompt "a standing corgi dog" --sds_guidance 1
+python Q21_image_optimization.py --prompt "a robot arm" --sds_guidance 0
+python Q21_image_optimization.py --prompt "a robot arm" --sds_guidance 1
+python Q21_image_optimization.py --prompt "a pikachu" --sds_guidance 0
+python Q21_image_optimization.py --prompt "a pikachu" --sds_guidance 1
+python Q21_image_optimization.py --prompt "a dragon" --sds_guidance 0
+python Q21_image_optimization.py --prompt "a dragon" --sds_guidance 1
