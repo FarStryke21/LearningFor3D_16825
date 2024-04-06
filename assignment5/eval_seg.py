fc31ed01e6902032af0751eb6b6857124839c228
Aman Chulawala <aman.chulawala@gmail.com>
2024-04-05 22:24:30 -0400

    Update eval_seg.py

--- assignment5/eval_seg.py
+++ assignment5/eval_seg.py
@@ -73,18 +73,13 @@ if __name__ == '__main__':
     #     viz_seg(test_data[i], test_label[i], "{}/seg/gt_{}_{}.gif".format(args.output_dir, args.exp_name, i), args.device)
     #     viz_seg(test_data[i], pred_label[i], "{}/seg/pred_{}_{}.gif".format(args.output_dir, args.exp_name, i), args.device)
 
+    accuracy = []
     for idx in tqdm(range(len(test_data))):
-        viz_seg(test_data[idx].cpu(), test_label[idx].cpu(), "{}/seg/gt_{}_{}.gif".format(args.output_dir, args.exp_name, idx), args.device)
-        viz_seg(test_data[idx].cpu(), pred_label[idx].cpu(), "{}/seg/pred_{}_{}.gif".format(args.output_dir, args.exp_name, idx), args.device)
+        test_accuracy = pred_label[idx].eq(test_label[idx].data).cpu().sum().item() / (test_label[idx].reshape((-1,1)).size()[0])
+        accuracy.append(test_accuracy)
+        # viz_seg(test_data[idx].cpu(), test_label[idx].cpu(), "{}/seg/gt_{}_{}.gif".format(args.output_dir, args.exp_name, idx), args.device)
+        # viz_seg(test_data[idx].cpu(), pred_label[idx].cpu(), "{}/seg/pred_{}_{}.gif".format(args.output_dir, args.exp_name, idx), args.device)
     
-    test_label = test_label.cpu().numpy()
-    pred_label = pred_label.cpu().numpy()
-
-    incorrect_labels = []
-    for i in range(len(test_label)):
-        if test_label[i] != pred_label[i]:
-            incorrect_labels.append(i)
-    
-    print("Incorrect labels: ", incorrect_labels)
+    print("Incorrect labels: ", accuracy)
 
 # test accuracy: 0.9028991896272285
